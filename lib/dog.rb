6e0c6ba43d2f05437745f0f7ee2c790e804465c0
Anthony George <ageorge530@gmail.com>
2020-06-10 07:38:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
-  # code goes here
+  
+  attr_accessor :mood, :owner
+  attr_reader :$FILENAME
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name 
+    @mood = "nervous"
+    @owner = owner
+    @all << self
+  end
+  
+  def self.all
+    @@all
+  end
+    
+end
 end
